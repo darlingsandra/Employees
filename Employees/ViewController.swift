07dffb0e91d3718c1148380f3f423fb19ffac0d8
darlingsandra <darlingsandra@yandex.ru>
2022-08-02 14:35:05 +0700

    Initial Commit

---
+++ Employees/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Employees
+//
+//  Created by Александра Широкова on 02.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
